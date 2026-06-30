4421a1912205f85f38940acda1684df06adfa4f0
Salsabila Julianti Virna Joffana <salsabilajulianti.v.j@gmail.com>
2026-06-30 21:10:45 +0700

    Fix Modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -52,29 +52,23 @@ mlflow.sklearn.autolog()
 
 # TRAINING MODEL
 
-with mlflow.start_run():
+model = LogisticRegression(max_iter=1000)
 
-    model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-    model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
-    y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
+print("\n===== HASIL EVALUASI =====")
+print(f"Accuracy  : {accuracy:.4f}")
+print(f"Precision : {precision:.4f}")
+print(f"Recall    : {recall:.4f}")
+print(f"F1 Score  : {f1:.4f}")
 
-    print("\n===== HASIL EVALUASI =====")
-    print(f"Accuracy  : {accuracy:.4f}")
-    print(f"Precision : {precision:.4f}")
-    print(f"Recall    : {recall:.4f}")
-    print(f"F1 Score  : {f1:.4f}")
-
-    print("\nClassification Report")
-    print(classification_report(y_test, y_pred))
-
-    print("\nConfusion Matrix")
-    print(confusion_matrix(y_test, y_pred))
-    
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
 
